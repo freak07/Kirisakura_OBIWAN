f82311402f1bedd4333a5fe56e36981bdbd57194
Masahiro Yamada <yamada.masahiro@socionext.com>
2019-03-07 16:27:14 -0800

    BACKPORT: linux/kernel.h: split *_MAX and *_MIN macros into <linux/limits.h>
    <linux/kernel.h> tends to be cluttered because we often put various sort
    of unrelated stuff in it.  So, we have split out a sensible chunk of
    code into a separate header from time to time.
    
    This commit splits out the *_MAX and *_MIN defines.
    
    The standard header <limits.h> contains various MAX, MIN constants
    including numerial limits.  [1]
    
    I think it makes sense to move in-kernel MAX, MIN constants into
    include/linux/limits.h.
    
    We already have include/uapi/linux/limits.h to contain some user-space
    constants.  I changed its include guard to _UAPI_LINUX_LIMITS_H.  This
    change has no impact to the user-space because
    scripts/headers_install.sh rips off the '_UAPI' prefix from the include
    guards of exported headers.
    
    [1] http://pubs.opengroup.org/onlinepubs/009604499/basedefs/limits.h.html
    
    Link: http://lkml.kernel.org/r/1549156242-20806-2-git-send-email-yamada.masahiro@socionext.com
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Cc: Alex Elder <elder@linaro.org>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Zhang Yanmin <yanmin.zhang@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 54d50897d544c874562253e2a8f70dfcad22afe8)
    [ salyzyn: moved include of linux/limits.h to solve module CRC differences ]
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 154668398
    Change-Id: Iae4259d122e289d6854edb221440a45be95ca015
    Signed-off-by: Chiawei Wang <chiaweiwang@google.com>
    (cherry picked from commit 2e2011f4d9626e3a99cd71993ff2212cc3da3e62)
    (cherry picked from commit d91b85db6417b8779dad59c7cc7dc5b57416bdfd)

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -2,7 +2,6 @@
 #ifndef _LINUX_KERNEL_H
 #define _LINUX_KERNEL_H
 
-
 #include <stdarg.h>
 #include <linux/linkage.h>
 #include <linux/stddef.h>
@@ -16,6 +15,7 @@
 #include <asm/byteorder.h>
 #include <uapi/linux/kernel.h>
 #include <linux/asusdebug.h>
+#include <linux/limits.h>
 
 #ifdef ASUS_ZS661KS_PROJECT
 enum DEVICE_HWID
@@ -152,34 +152,6 @@ extern int permissive_enable;
 // ASUS_BSP --- get permissive status
 
 #define FORCE_RAMDUMP_FEATURE 1
-
-#define USHRT_MAX	((u16)(~0U))
-#define SHRT_MAX	((s16)(USHRT_MAX>>1))
-#define SHRT_MIN	((s16)(-SHRT_MAX - 1))
-#define INT_MAX		((int)(~0U>>1))
-#define INT_MIN		(-INT_MAX - 1)
-#define UINT_MAX	(~0U)
-#define LONG_MAX	((long)(~0UL>>1))
-#define LONG_MIN	(-LONG_MAX - 1)
-#define ULONG_MAX	(~0UL)
-#define LLONG_MAX	((long long)(~0ULL>>1))
-#define LLONG_MIN	(-LLONG_MAX - 1)
-#define ULLONG_MAX	(~0ULL)
-#define SIZE_MAX	(~(size_t)0)
-#define PHYS_ADDR_MAX	(~(phys_addr_t)0)
-
-#define U8_MAX		((u8)~0U)
-#define S8_MAX		((s8)(U8_MAX>>1))
-#define S8_MIN		((s8)(-S8_MAX - 1))
-#define U16_MAX		((u16)~0U)
-#define S16_MAX		((s16)(U16_MAX>>1))
-#define S16_MIN		((s16)(-S16_MAX - 1))
-#define U32_MAX		((u32)~0U)
-#define S32_MAX		((s32)(U32_MAX>>1))
-#define S32_MIN		((s32)(-S32_MAX - 1))
-#define U64_MAX		((u64)~0ULL)
-#define S64_MAX		((s64)(U64_MAX>>1))
-#define S64_MIN		((s64)(-S64_MAX - 1))
 
 #define STACK_MAGIC	0xdeadbeef
 
