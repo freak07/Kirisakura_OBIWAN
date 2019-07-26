8435cfa69a8bc726cbfefc2f354d092a9d94f0d2
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-26 16:50:21 +0900

    Revert "dma-buf: Use monotonic clock for buffer time tracking"
    This reverts commit 72483f353a5eda2f28283f8c736c75fa93bc3503.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    (cherry picked from commit c5a901a1736d15263fd41ce358f6ff0a23147ed1)
    (cherry picked from commit 01deec6f065b8e1260162a13c636ab66f3b35e2b)
    (cherry picked from commit b00421a67f3bb3b4ea5e112cf0c3a59bb5bbfb16)
    (cherry picked from commit 6db77cd6bd6060564cfb8ae9c7f22d68ab26d687)
    (cherry picked from commit 5fa790f6ea7dd036c81f08097a2237bf8f8667c3)
    (cherry picked from commit c02f4a39b4201e56166de68d24f80c57f05b3387)
    (cherry picked from commit d6f0152a002f6e3a6555d929c7c89302003ee5db)
    (cherry picked from commit e8aafeefe0881c130c31027418ed3e907191011d)
    (cherry picked from commit d303957a5790df13d4afb58956b18dfa37b54c65)
    (cherry picked from commit 9d13d678aeca867dbd60997ac524f57f4f01aa63)
    (cherry picked from commit 09f5d12e4bb01ffba5b9efbd20c9c45862aa785b)
    (cherry picked from commit 05340e5130423795ad3535140ffe3c27bbc09cc2)
    (cherry picked from commit 05ed36aec6aee79ce23e6cc08547148b29062341)
    (cherry picked from commit 17c60a5898fc21061e9a343b393a97adec6e1283)
    (cherry picked from commit 8fd61b45e912417fd04c87b2d7b89b5925fb94b6)
    (cherry picked from commit ce503f025ed87c5331e2586836cb8a22765a2797)

--- include/linux/dma-buf.h
+++ include/linux/dma-buf.h
@@ -394,7 +394,6 @@ typedef int (*dma_buf_destructor)(struct dma_buf *dmabuf, void *dtor_data);
  * @vmap_ptr: the current vmap ptr if vmapping_counter > 0
  * @exp_name: name of the exporter; useful for debugging.
  * @name: unique name for the buffer
- * @ktime: time (in jiffies) at which the buffer was born
  * @owner: pointer to exporter module; used for refcounting when exporter is a
  *         kernel module.
  * @list_node: node for dma_buf accounting and debugging.
@@ -423,7 +422,7 @@ struct dma_buf {
 	void *vmap_ptr;
 	const char *exp_name;
 	char *name;
-	ktime_t ktime;
+	struct timespec ctime;
 	struct module *owner;
 	struct list_head list_node;
 	void *priv;
