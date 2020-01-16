65568cb3f0f549f9e23f2d323aafe5135db763d1
Ondrej Mosnacek <omosnace@redhat.com>
2020-01-16 19:45:57 +0100

    selinux: fix wrong buffer types in policydb.c
    Two places used u32 where there should have been __le32.
    
    Fixes sparse warnings:
      CHECK   [...]/security/selinux/ss/services.c
    [...]/security/selinux/ss/policydb.c:2669:16: warning: incorrect type in assignment (different base types)
    [...]/security/selinux/ss/policydb.c:2669:16:    expected unsigned int
    [...]/security/selinux/ss/policydb.c:2669:16:    got restricted __le32 [usertype]
    [...]/security/selinux/ss/policydb.c:2674:24: warning: incorrect type in assignment (different base types)
    [...]/security/selinux/ss/policydb.c:2674:24:    expected unsigned int
    [...]/security/selinux/ss/policydb.c:2674:24:    got restricted __le32 [usertype]
    [...]/security/selinux/ss/policydb.c:2675:24: warning: incorrect type in assignment (different base types)
    [...]/security/selinux/ss/policydb.c:2675:24:    expected unsigned int
    [...]/security/selinux/ss/policydb.c:2675:24:    got restricted __le32 [usertype]
    [...]/security/selinux/ss/policydb.c:2676:24: warning: incorrect type in assignment (different base types)
    [...]/security/selinux/ss/policydb.c:2676:24:    expected unsigned int
    [...]/security/selinux/ss/policydb.c:2676:24:    got restricted __le32 [usertype]
    [...]/security/selinux/ss/policydb.c:2681:32: warning: incorrect type in assignment (different base types)
    [...]/security/selinux/ss/policydb.c:2681:32:    expected unsigned int
    [...]/security/selinux/ss/policydb.c:2681:32:    got restricted __le32 [usertype]
    [...]/security/selinux/ss/policydb.c:2701:16: warning: incorrect type in assignment (different base types)
    [...]/security/selinux/ss/policydb.c:2701:16:    expected unsigned int
    [...]/security/selinux/ss/policydb.c:2701:16:    got restricted __le32 [usertype]
    [...]/security/selinux/ss/policydb.c:2706:24: warning: incorrect type in assignment (different base types)
    [...]/security/selinux/ss/policydb.c:2706:24:    expected unsigned int
    [...]/security/selinux/ss/policydb.c:2706:24:    got restricted __le32 [usertype]
    [...]/security/selinux/ss/policydb.c:2707:24: warning: incorrect type in assignment (different base types)
    [...]/security/selinux/ss/policydb.c:2707:24:    expected unsigned int
    [...]/security/selinux/ss/policydb.c:2707:24:    got restricted __le32 [usertype]
    
    Signed-off-by: Ondrej Mosnacek <omosnace@redhat.com>
    Reviewed-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    (cherry picked from commit 8d94e93dd22d97112da27aaf160cdea0740ada15)
    (cherry picked from commit 8c95d8e5d6bae210ec0fb2970c2d50b0d864c5e4)
    (cherry picked from commit 492c4114ccd0c2d788c80dec6c9656f2270b3669)

--- security/selinux/ss/policydb.c
+++ security/selinux/ss/policydb.c
@@ -2665,7 +2665,7 @@ static int role_trans_write(struct policydb *p, void *fp)
 {
 	struct role_trans *r = p->role_tr;
 	struct role_trans *tr;
-	u32 buf[3];
+	__le32 buf[3];
 	size_t nel;
 	int rc;
 
@@ -2697,7 +2697,7 @@ static int role_trans_write(struct policydb *p, void *fp)
 static int role_allow_write(struct role_allow *r, void *fp)
 {
 	struct role_allow *ra;
-	u32 buf[2];
+	__le32 buf[2];
 	size_t nel;
 	int rc;
 
