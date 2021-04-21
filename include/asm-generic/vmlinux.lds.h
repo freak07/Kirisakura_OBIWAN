dca10380e2a312ad42fd7a1e37067a0b8522a6a3
freak07 <michalechner92@googlemail.com>
2021-04-21 20:29:33 +0200

    gMerge tag 'v4.19.188' into master_caf_cam_LTO_linuxstable
    This is the 4.19.188 stable release
    
    (cherry picked from commit 931d6de8d2e979d3bbdd856cebfc2f92dce01306)

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -485,6 +485,15 @@
 	}
 
 /*
+ * Non-instrumentable text section
+ */
+#define NOINSTR_TEXT							\
+		ALIGN_FUNCTION();					\
+		__noinstr_text_start = .;				\
+		*(.noinstr.text)					\
+		__noinstr_text_end = .;
+
+/*
  * .text section. Map to function alignment to avoid address changes
  * during second ld run in second ld pass when generating System.map
  *
@@ -497,8 +506,9 @@
 		*(.text.hot .text.hot.*)				\
 		*(TEXT_MAIN .text.fixup)				\
 		*(.text.unlikely .text.unlikely.*)			\
-		*(.text.unknown .text.unknown.*)	\
+		*(.text.unknown .text.unknown.*)			\
 		*(TEXT_CFI_MAIN) 					\
+		NOINSTR_TEXT						\
 		*(.text..refcount)					\
 		*(.text..ftrace)					\
 		*(.ref.text)						\
